e008f609aced79a9b8adbb7f6e3f74b79a93cddd
Hayko93 <51296724+Hayko93@users.noreply.github.com>
2019-06-03 23:45:30 +0400

    Update Main.java

--- src/am/client/market/Main.java
+++ src/am/client/market/Main.java
@@ -3,5 +3,6 @@ package am.client.market;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello world 22222");
     }
 }
